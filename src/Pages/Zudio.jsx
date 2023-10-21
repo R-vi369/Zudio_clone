66212f702ed8ac2d0711eecc93dc6336326be330
Ravi <sama369.rk@gmail.com>
2023-10-21 12:53:33 +0530

    zudio images completed

--- src/Pages/Zudio.jsx
+++ src/Pages/Zudio.jsx
@@ -1,19 +1,77 @@
 import React from 'react'
+import zudioShateimgs01 from '../assets/shareImg/zudioShateimgs01.jpg'
+import zudioShateimgs02 from '../assets/shareImg/zudioShateimgs02.jpg'
+import zudioShateimgs03 from '../assets/shareImg/zudioShateimgs03.jpg'
+import zudioShateimgs04 from '../assets/shareImg/zudioShateimgs04.jpg'
+import zudioShateimgs05 from '../assets/shareImg/zudioShateimgs05.jpg'
+import zudioShateimgs06 from '../assets/shareImg/zudioShateimgs06.jpg'
+import zudioShateimgs07 from '../assets/shareImg/zudioShateimgs07.jpg'
+import zudioShateimgs08 from '../assets/shareImg/zudioShateimgs08.jpg'
+import zudioShateimgs09 from '../assets/shareImg/zudioShateimgs09.jpg'
+import zudioShateimgs10 from '../assets/shareImg/zudioShateimgs10.jpg'
+import zudioShateimgs11 from '../assets/shareImg/zudioShateimgs11.jpg'
+import zudioShateimgs12 from '../assets/shareImg/zudioShateimgs12.jpg'
+import zudioShateimgs13 from '../assets/shareImg/zudioShateimgs13.jpg'
+import zudioShateimgs14 from '../assets/shareImg/zudioShateimgs14.jpg'
+import zudioShateimgs15 from '../assets/shareImg/zudioShateimgs15.jpg'
+import zudioShateimgs16 from '../assets/shareImg/zudioShateimgs16.jpg'
+import zudioShateimgs17 from '../assets/shareImg/zudioShateimgs17.jpg'
+import zudioShateimgs18 from '../assets/shareImg/zudioShateimgs18.jpg'
+import zudioShateimgs19 from '../assets/shareImg/zudioShateimgs19.jpg'
+import zudioShateimgs20 from '../assets/shareImg/zudioShateimgs20.jpg'
+import zudioShateimgs21 from '../assets/shareImg/zudioShateimgs21.jpg'
+import zudioShateimgs22 from '../assets/shareImg/zudioShateimgs22.jpg'
+import zudioShateimgs23 from '../assets/shareImg/zudioShateimgs23.jpg'
+import zudioShateimgs24 from '../assets/shareImg/zudioShateimgs24.jpg'
+import Footer from '../components/Footer'
 
 const Zudio = () => {
   return (
-    <div className="zudio">
+    <>
 
-<h1>#zudio</h1>
-<p>SHARE YOUR LOOK</p>
+      <div className="bg-white h-screen flex items-center flex-col w-full ">
+        <div className='text-black flex flex-col '>
+
+          <h1 className='text-7xl pt-3'>#zudio</h1>
+          <p className='text-2xl py-2'>SHARE YOUR LOOK</p>
+        </div>
+
+        <div className='flex m-20 flex-wrap justify-center gap-4 '>
+
+          <img src={zudioShateimgs01} alt="" />
+          <img src={zudioShateimgs02} alt="" />
+          <img src={zudioShateimgs03} alt="" />
+          <img src={zudioShateimgs04} alt="" />
+          <img src={zudioShateimgs05} alt="" />
+          <img src={zudioShateimgs06} alt="" />
+          <img src={zudioShateimgs07} alt="" />
+          <img src={zudioShateimgs08} alt="" />
+          <img src={zudioShateimgs09} alt="" />
+          <img src={zudioShateimgs10} alt="" />
+          <img src={zudioShateimgs11} alt="" />
+          <img src={zudioShateimgs12} alt="" />
+          <img src={zudioShateimgs13} alt="" />
+          <img src={zudioShateimgs14} alt="" />
+          <img src={zudioShateimgs15} alt="" />
+          <img src={zudioShateimgs16} alt="" />
+          <img src={zudioShateimgs17} alt="" />
+          <img src={zudioShateimgs18} alt="" />
+          <img src={zudioShateimgs19} alt="" />
+          <img src={zudioShateimgs20} alt="" />
+          <img src={zudioShateimgs21} alt="" />
+          <img src={zudioShateimgs22} alt="" />
+          <img src={zudioShateimgs23} alt="" />
+          <img src={zudioShateimgs24} alt="" />
+        </div>
 
 
 
+  <div className='w-full'>
 
-
-
-
-    </div>
+        <Footer />
+  </div>
+      </div>
+    </>
   )
 }
 
