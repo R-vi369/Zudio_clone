7dd76323a3fb787065386e64d66a97902105105d
Ravi <sama369.rk@gmail.com>
2023-10-28 20:31:35 +0530

    working on zudio location

--- src/Pages/ZudioNearYou.jsx
+++ src/Pages/ZudioNearYou.jsx
@@ -1,31 +1,37 @@
 import React from 'react'
 import '../styles/ZudioNearYou.scss'
-import {Link} from 'react-router-dom'
-import {MdKeyboardDoubleArrowRight} from 'react-icons/md'
+import { Link } from 'react-router-dom'
+import { MdKeyboardDoubleArrowRight } from 'react-icons/md'
 const ZudioNearYou = () => {
   return (
     <>
-    <div className="zudioNearYou">
+      <div className="zudioNearYou">
+        <div className='HSL' >
+          <Link className='hsl_link' to='/'>Home</Link>
+          <MdKeyboardDoubleArrowRight fontSize="15px" />
+          <Link className='hsl_link' to='/ZudioNearYou'>Store Locator</Link>
+          <MdKeyboardDoubleArrowRight fontSize="15px" />
+        </div>
 
-    <div className='HSL' >
-       
-       <Link className='hsl_link'  to='/'>Home</Link>
-       <MdKeyboardDoubleArrowRight fontSize="15px"/>
-        <Link className='hsl_link' to='/ZudioNearYou'>Store Locator</Link>
-        <MdKeyboardDoubleArrowRight fontSize="15px"/>
+        <div className="location">
+          <h1>Store locator</h1>
+          <div className="findZudioStore">
+            <h1>find the zudio store closest to you</h1>
+            <div className="search">
+
+              <input type="text" />
+              g
+              <input type="text" />
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
 
 
-    </div>
 
-
-    <div>
-      <p>CURRENTLY IN 42 CITIES IN INDIA, COMING TO YOUR CITY VERY SOON</p>
-    </div>
-    
-    </div>
-    
-    
-    
     </>
   )
 }
