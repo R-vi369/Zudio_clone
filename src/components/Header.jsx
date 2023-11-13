2888d5b10c47c2ea7aacbfbbaf24060ccdeb1864
Ravi <sama369.rk@gmail.com>
2023-11-13 19:15:59 +0530

    added nav icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/Header.scss'
 import logo from '../assets/logo.png'
+import { AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 const Header = () => {
+    const [isToggled, setIsToggled] = useState(false);
+
+    const toggleBtn =()=>{
+       setIsToggled(!isToggled);
+    }
     return (
 
         <div className="header">
+            <div className="toggleButton" onClick={toggleBtn}>
+              {isToggled? <AiOutlineMenu/>: <AiOutlineClose/>}
+            </div>
             <div className="headerLeft">
                 <Link to='/'>
-                
-              <img src={logo} alt="" />
+
+                    <img src={logo} alt="" />
                 </Link>
             </div>
-            <div className="headerRight">
+            <div className="headerRight " >
 
-                <Link  className='headerLinks' to='/Zstories'>Z Stories</Link>
+                <Link className='headerLinks' to='/Zstories'>Z Stories</Link>
                 <Link className='headerLinks' to='/Zudio'>#Zudio</Link>
                 <Link className='headerLinks' to='/ZudioNearYou'>Zudio Near You</Link>
                 <Link className='headerLinks' to='/Zworld'>Z World</Link>
