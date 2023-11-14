6631f66982b531db2f03edc43766b8cfe88a3e45
Ravi <sama369.rk@gmail.com>
2023-11-14 12:49:57 +0530

    pagination added in zudio near me section

--- src/Pages/Location.jsx
+++ src/Pages/Location.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import '../styles/Location.scss'
 import {AiOutlineDoubleRight} from 'react-icons/ai'
-const Location = () => {
+const Location = ({name, addressDetail}) => {
   return (
     <>
-    <div className="w-[30%] mx-4 my-1 p-4 bg-[#f3f3f3] ">
-        <h1 className='font-bold text-[1.2rem] p-2 uppercase'>Zudio - ahmedabar, satyamev elite</h1>
-        <h1 className='uppercase font-bold text-[0.9rem] p-2'>Address</h1>
-        <p>GF Shop No 101 to 106, Satyamev Elite, Bopal Bridge, Bopal Ambli Road, Opp BRTS , Ahmedabad - 380058 , Gujarat</p>
-        <button className='border-2 bg-black text-white px-4 py-2 flex items-center'>
-           <p className=''> more details </p>
+    <div className="w-[33%] m-[1px] p-4 bg-[#f3f3f3] ">
+        <h1 className='font-bold text-[1.2rem] p-2 uppercase'>Zudio -{name}</h1>
+        <h1 className='uppercase font-bold text-[0.9rem] p-2 '>Address</h1>
+        <p className='text-[0.8rem] px-2 py-[2px]'>{addressDetail}</p>
+        <button className=' mt-2 border-2 bg-black text-white p-2 flex items-center'>
+           <p className='font-bold uppercase text-[0.8rem]'> more details </p>
             <AiOutlineDoubleRight className='text-[0.9rem]'/>
         </button>
     </div>
--- src/Pages/ZudioNearYou.jsx
+++ src/Pages/ZudioNearYou.jsx
@@ -3,6 +3,8 @@ import '../styles/ZudioNearYou.scss'
 import { Link } from 'react-router-dom'
 import { MdKeyboardDoubleArrowRight } from 'react-icons/md'
 import Location from './Location'
+import Pagination from './Pagination'
+import Footer from '../components/Footer'
 const ZudioNearYou = () => {
   return (
     <>
@@ -18,39 +20,59 @@ const ZudioNearYou = () => {
           <h1>Store locator</h1>
           <div className="findZudioStore">
             <h1>find the zudio store closest to you</h1>
-            <div className="search">
+            <div className="search flex gap-6 ">
 
-              <input type="text" />
-              g
-              <input type="text" />
+              <input type="text" placeholder='Select State' className='w-[18rem] h-[2.4rem] px-4 text-black'/>
+           
+              <input type="text" placeholder='Select City' className='w-[18rem] h-[2.4rem] px-4 text-black' />
             </div>
 
           </div>
 
 
         </div>
-       
-
-         <div className="AllStore">ALL STORES (437)</div>
 
 
-
-   <div className=" flex justify-center items-center flex-wrap">
-
-  <Location/>
-  <Location/>
-  <Location/>
-  <Location/>
-  <Location/>
-  <Location/>
-
-   </div>
+        <div className="AllStore">ALL STORES (437)</div>
 
 
 
+        <div className=" flex m-5  flex-wrap">
+
+          <Location name='AHMEDABAD, SATYAMEV ELITE' addressDetail='GF Shop No 101 to 106, Satyamev Elite, Bopal Bridge, Bopal Ambli Road, Opp BRTS , Ahmedabad - 380058 , Gujarat' />
+
+
+          <Location name='INDORE, PHOENIX CITADEL MALL' addressDetail='Survey No. 248/1/1, Next to Mumbai Agra Bypass Village Khajrana , Indore - 452016 , Madhya Pradesh' />
+
+          <Location name='JALANDHAR, CURO HIGH STREET' addressDetail='66 feet road, Near Urban Estate, Phase- II , Jalandhar - 144022 , Punjab' />
+
+
+          <Location name='AHMEDABAD, SHALIN SQUARE' addressDetail='Hathijan Circle, Vinzol , Ahmedabad - 382445 , Gujarat' />
+
+          <Location name='MUZAFFARPUR, SHIV MITRA TOWER' addressDetail='Kalambagh Road, Near Reliance Petroleum, Gannipur, PS Kazi, Anchal Mushari , Muzaffarpur - 842002 , Bihar' />
+
+          <Location name='AHMEDABAD, SATYAMEV ELITE' addressDetail='GF Shop No 101 to 106, Satyamev Elite, Bopal Bridge, Bopal Ambli Road, Opp BRTS , Ahmedabad - 380058 , Gujarat' />
 
 
 
+          <Location name='NELLORE, GT ROAD' addressDetail='D.No.24/2/436/2, Saraswati Nagar, KVR Petrol Bunk,Dargamitta , Nellore - 524003 , Andhra Pradesh' />
+
+
+          <Location name='BENGALURU, VIGNAN NAGAR' addressDetail='Site No. 3 & 4, BE, Katta No. 301/143/3/A, Vibhutipura Village, K. R. Puram, Hobli , Bengaluru - 560043 , Karnataka' />
+
+
+          <Location name='AHMEDABAD, SATYAMEV ELITE' addressDetail='GF Shop No 101 to 106, Satyamev Elite, Bopal Bridge, Bopal Ambli Road, Opp BRTS , Ahmedabad - 380058 , Gujarat' />
+
+
+        </div>
+
+        <div className="flex  justify-center">
+
+        <Pagination/>
+        </div>
+
+
+    <Footer/>
 
 
       </div>
